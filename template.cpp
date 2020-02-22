134523b8895c35afeccd97bc58674a1d6dfa9abe
Md Fahad <md.fahad742@gmail.com>
2020-02-23 00:20:47 +0530

    Create template.cpp
    CP template

---
+++ template.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+ 
+#define ll long long
+#define pb push_back
+
+#define eb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define db double
+#define pll pair<ll, ll>
+#define pdb pair<db, db>
+#define ALL(a) (a).begin(),(a).end()
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define fornab(i,a,b) for(int i=a;i<b;i++)
+#define clr(x) memset(x, 0, sizeof(x))
+#define count_bit(x) __builtin_popcount(x)
+#define endl "\n"
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+#define trace(v) for(auto it=v.begin();it!=v.end();it++)cerr<<*it<<" ";cerr<<endl;
+#define eps 1e-12
+const ll size = 505;
+const ll mod = 1e9 + 7;
+
+
+int main() {
+    IOS;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    
+ 
+    return 0;
+}
