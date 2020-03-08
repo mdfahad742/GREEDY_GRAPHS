efb8c212014b4fba50ed7abf1d0ca25a68821595
Md Fahad <md.fahad742@gmail.com>
2020-03-09 00:51:31 +0530

    added features

--- template.cpp
+++ template.cpp
@@ -1,10 +1,11 @@
+// fahadmd16
 #include<bits/stdc++.h>
 #include <algorithm>
 using namespace std;
  
 #define ll long long
 #define pb push_back
-
+#define pf push_front
 #define eb emplace_back
 #define mp make_pair
 #define ff first
@@ -13,33 +14,40 @@ using namespace std;
 #define pll pair<ll, ll>
 #define pdb pair<db, db>
 #define ALL(a) (a).begin(),(a).end()
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define forn(i, n) for (int i = 0; i < int(n); i++)
-#define forn1(i, n) for (int i = 1; i < int(n); i++)
-#define fornab(i,a,b) for(int i=a;i<b;i++)
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define forn(i, n) for (ll i = 0; i < n; i++)
+#define forn1(i, n) for (ll i = 1; i < n; i++)
+#define fornab(i,a,b) for(ll i=a;i<=b;i++)
 #define clr(x) memset(x, 0, sizeof(x))
 #define count_bit(x) __builtin_popcount(x)
 #define endl "\n"
-#define trace1(x)                cerr<<#x<<": "<<x<<endl
-#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
-#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
-#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
-#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
-#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
-#define trace(v) for(auto it=v.begin();it!=v.end();it++)cerr<<*it<<" ";cerr<<endl;
+#define trace1(x)                cout<<#x<<": "<<x<<endl
+#define trace2(x, y)             cout<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cout<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+#define trace(v) for(auto it=v.begin();it!=v.end();it++)cout<<*it<<" ";cout<<endl;
 #define eps 1e-12
-const ll size = 505;
+#define nedl endl
+const ll size = 805;
 const ll mod = 1e9 + 7;
 
 
+
+ 
 int main() {
     IOS;
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
-    
-    
- 
+    ll t=1;cin>>t;
+    while(t--){
+       
+
+       
+    }
+
     return 0;
 }
