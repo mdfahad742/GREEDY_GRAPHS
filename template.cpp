4f4a84c5f4a14fff36ad5b6ff217b3ca3149d68f
Md Fahad <md.fahad742@gmail.com>
2020-08-09 16:54:27 +0530

    updated template

--- template.cpp
+++ template.cpp
@@ -1,53 +1,100 @@
-// fahadmd16
+// fahadmd16(IIEST Shibpur)
 #include<bits/stdc++.h>
-#include <algorithm>
 using namespace std;
- 
-#define ll long long
-#define pb push_back
-#define pf push_front
-#define eb emplace_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define db double
-#define pll pair<ll, ll>
-#define pdb pair<db, db>
-#define ALL(a) (a).begin(),(a).end()
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-#define forn(i, n) for (ll i = 0; i < n; i++)
-#define forn1(i, n) for (ll i = 1; i < n; i++)
-#define fornab(i,a,b) for(ll i=a;i<=b;i++)
-#define clr(x) memset(x, 0, sizeof(x))
-#define count_bit(x) __builtin_popcount(x)
-#define endl "\n"
-#define trace1(x)                cout<<#x<<": "<<x<<endl
-#define trace2(x, y)             cout<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
-#define trace3(x, y, z)          cout<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
-#define trace4(a, b, c, d)       cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
-#define trace5(a, b, c, d, e)    cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
-#define trace6(a, b, c, d, e, f) cout<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
-#define trace(v) for(auto it=v.begin();it!=v.end();it++)cout<<*it<<" ";cout<<endl;
-#define eps 1e-12
-#define nedl endl
-const ll size = 805;
+
+#define ll                                      long long
+#define pb                                      push_back
+#define pf                                      push_front
+#define eb                                      emplace_back
+#define mp                                      make_pair
+#define ff                                      first
+#define ss                                      second
+#define db                                      double
+#define pll                                     pair<ll, ll>
+#define ALL(a)                                  (a).begin(),(a).end()
+#define endl                                    "\n"
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define forn1(i, n) for (int i = 1; i < int(n); i++)
+#define ford(i, n) for (int i = int(n) - 1; i >= 0; i--)
+#define ford1(i, n) for (int i = int(n) - 1; i >= 1; i--)
+#define forab(i, a, b) for(int i = int(a); i < int(b); i++)
+#define forabd(i, a, b) for(int i = int(a); i >= int(b); i--)
+
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); cerr.tie(0)
+
+/// ---------------------------- (Debug) ------------------------------
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cout << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifndef ONLINE_JUDGE
+~debug() { cout << endl; }
+eni(!=) cout << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+/// --------------------------------------------------------------------
+
+
+const ll sz = 1e5 + 5;
 const ll mod = 1e9 + 7;
+const db PI = acos(-1);
+const db eps = 1e-10;
+const ll INF = 1e18 + 5;
+
+
+typedef vector<ll> VL;
+typedef vector<pll > VLL;
+typedef vector<vector<ll> > VVL;
+typedef vector<vector<vector<ll> > > VVVL;
 
 
 
- 
+ll power(ll n, ll k, ll m) {
+	ll ans = 1;
+	while (k) {
+		if (k & 1){
+			ans = (ans * n) % m;
+			k--;
+		}
+		else{
+			n = (n * n) % m;
+			k /= 2;
+		}
+	}
+	return ans;
+}
+
+
+void solver() {
+	
+
+}
+
 int main() {
-    IOS;
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-    ll t=1;cin>>t;
-    while(t--){
-       
-
-       
-    }
-
-    return 0;
+	IOS;
+	ll T = 1; cin >> T;
+	forn1(i, T + 1) {
+		solver();
+	}
+	return 0;
 }
